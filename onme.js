3473a3d9995fffcca138a158769e3c035d0df627
mucahiddayan <muecahid@dayan.one>
2017-09-20 10:57:08 +0200

    master version mit onscreen.

--- onme.js
+++ onme.js
@@ -84,10 +84,8 @@
                 // console.log(index,positions[index],$(this).scrollTop());
                 // self.log(index,positions[index],$(this).scrollTop(),'diffs:',diffs,'pos:',positions); */
                 
-                let tmp  = self.getOnScreen(window.scrollY),
-                onScreen = tmp.removed,
-                stay     = tmp.stay;
-                self.activate(onScreen,stay);
+                let tmp  = self.getOnScreen(window.scrollY);               
+                self.activate(tmp.on,tmp.out);
                 
                 /* $(items).eq(index).addClass('active');
                 $(items).eq(index).siblings('.onme.nav-item').removeClass('active'); */
@@ -109,11 +107,11 @@
             }
             </style>`);
         };
-
-        this.activate = function(els,deac){
-            console.log(els,deac);
-            $.each(els,function(){$(this).addClass('active')});
-            $.each(deac,function(){$(this).removeClass('active')});
+        
+        this.activate = function(on,out){
+            console.log(on,out);
+            $.each(on,function(){$(this).addClass('active')});
+            $.each(out,function(){$(this).removeClass('active')});
         }
         
         this.getOnScreen = function(scrollY){
@@ -122,31 +120,30 @@
                 return  e >= scrollY && e <= scrollY + wInner;
             });
             let first = positions.findIndex(e=> e == self.min(onScreen));
-            // let last = positions.findIndex(e=> e == self.max(onScreen));
             first = first == -1?items.length-1:first;
-            console.group('log');
+            /* console.group('log');
             console.log('onscreen',onScreen);
             console.log('onscreen length',onScreen.length);
             console.log('min of onscreen',self.min(onScreen));
             console.log('first',first);
             // console.log('last',last);
-            console.log('removed',temp.splice(first,onScreen.length), typeof temp.splice(first,onScreen.length));
-            console.log('temp',temp,typeof temp);
-            console.groupEnd('log');
+            console.log('removed',temp.splice(first,onScreen.length), Array.isArray(temp.splice(first,onScreen.length)));
+            console.log('temp',temp,Array.isArray(temp));
+            console.groupEnd('log'); */
             return { 
-                removed: temp.splice(first,onScreen.length),
-                stay   : temp
+                on  : temp.splice(first,onScreen.length),
+                out : temp
             };        
         }
         
         this.min = function(arr){
             return arr.sort((a,b)=>a>b)[0];
         }
-
+        
         this.max = function(arr){
             return arr.sort((a,b)=>a<b)[0];
         }
-
+        
         this.goTo = function(topx){
             $('html,body').stop().animate({scrollTop:topx}, 500, 'swing');
         };
@@ -164,3 +161,19 @@
         return this;
     }
 })(jQuery)
+
+function addJQuery(){
+    if(typeof $ == 'undefined' && typeof jQuery == 'undefined'){
+        let head = document.getElementsByTagName('head')[0];
+        let jQuery = document.createElement('script');
+        jQuery.src = 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js';
+        jQuery.type = 'text/javascript';
+        head.appendChild(jQuery);
+    }else{
+        console.info('Good to know that you use jQuery');
+    }
+}
+
+document.addEventListener('DOMContentLoaded',()=>{
+    addJQuery();
+});
